2be3c1cb2db9fe771aa653ba51532903ceaca190
TejasBhovad <tejasbhovad@gmail.com>
2024-01-08 19:44:05 +0530

    style: added custom palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
   prefix: "",
   theme: {
@@ -51,6 +51,11 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        primary: "#96CDFB",
+        secondary: "#9B78EE",
+        accent: "#D4B7F7",
+        background: "#F3F9FE",
+        text: "#02090F",
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -74,4 +79,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
