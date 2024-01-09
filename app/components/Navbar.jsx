a4700da66bf97f920f1a97e8c9d6e04ed91fe45b
Tejas <tejasbhovad@gmail.com>
2024-01-09 17:41:13 +0530

    style: removed boilerplate

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-16 bg-red-400 flex">
+      <div className="w-1/3 h-full w-full bg-green-300"></div>
+      <div className="w-1/3 h-full w-full bg-blue-300"></div>
+      <div className="w-1/3 h-full w-full bg-green-300"></div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "@/app/components/Navbar";
+
+const Home = () => {
+  return (
+    <div className="w-full h-full bg-red-600">
+      <Navbar />
+      <div className="w-full h-full bg-green-600">hhhhf</div>
+    </div>
+  );
+};
+
+export default Home;
